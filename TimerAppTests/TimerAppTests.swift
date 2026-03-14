43eebd34523302b6b6e02435c44aa33118996d9c
Rajesh Mani <rajeshm79@outlook.com>
2026-03-14 17:20:57 +0530

    Initial Commit

---
+++ TimerAppTests/TimerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TimerAppTests.swift
+//  TimerAppTests
+//
+//  Created by Rajesh Mani on 14/03/26.
+//
+
+import Testing
+@testable import TimerApp
+
+struct TimerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
